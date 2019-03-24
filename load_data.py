8ea31394162af71563938eeab58d55ecb1f526c6
Josh Feldman <joshfeldman95@gmail.com>
2019-03-24 11:45:07 -0400

    implemented decomposable attn model

---
+++ load_data.py
@@ -0,0 +1,44 @@
+import torch
+# Text text processing library and methods for pretrained word embeddings
+import torchtext
+from torchtext.vocab import Vectors, GloVe
+
+# Named Tensor wrappers
+from namedtensor import ntorch, NamedTensor
+from namedtensor.text import NamedField
+import random
+
+def load(device = 'cpu',
+              pretrained_embedding = 'glove.6B.300d',
+              embedding_dim = 300,
+              embedding_num = 100):
+    # Our input $x$
+    TEXT = NamedField(names=('seqlen',))
+
+    # Our labels $y$
+    LABEL = NamedField(sequential=False, names=())
+
+    # create train val test split
+    train, val, test = torchtext.datasets.SNLI.splits(TEXT, LABEL)
+
+    # build vocabs
+    TEXT.build_vocab(train)
+    LABEL.build_vocab(train)
+
+    # create iters
+    train_iter, val_iter, test_iter = torchtext.data.BucketIterator.splits(
+    (train, val, test), batch_size=16, device=torch.device(device), repeat=False)
+
+    # Build the vocabulary with word embeddings
+    # Out-of-vocabulary (OOV) words are hashed to one of 100 random embeddings each
+    # initialized to mean 0 and standarad deviation 1 (Sec 5.1)
+    unk_vectors = [torch.randn(embedding_dim) for _ in range(embedding_num)]
+    TEXT.vocab.load_vectors(vectors=pretrained_embedding, unk_init=lambda x:random.choice(unk_vectors))
+
+    # normalized to have l_2 norm of 1
+    vectors = TEXT.vocab.vectors
+    vectors = vectors / vectors.norm(dim=1,keepdim=True)
+    vectors = NamedTensor(vectors, ('word', 'embedding'))
+    TEXT.vocab.vectors = vectors
+
+    return train_iter, val_iter, test_iter, TEXT, LABEL
---
+++ models.py
@@ -0,0 +1,76 @@
+import torch
+# Text text processing library and methods for pretrained word embeddings
+import torchtext
+from torchtext.vocab import Vectors, GloVe
+
+# Named Tensor wrappers
+from namedtensor import ntorch, NamedTensor
+from namedtensor.text import NamedField
+import random
+
+class AttentionModel(ntorch.nn.Module):
+    def __init__(self,
+                 TEXT, LABEL,
+                 hidden_attn, hidden_aligned,
+                 intra_attn = False, hidden_intra_attn = 200,
+                 dropout = 0.5, device = 'cpu', freeze_emb = True):
+        super().__init__()
+        self.pretrained_emb = TEXT.vocab.vectors.to(device)
+        self.embedding = ntorch.nn.Embedding.from_pretrained(self.pretrained_emb.values, freeze=freeze_emb).spec('seqlen','embedding')
+        emb_dim = self.pretrained_emb.shape['embedding']
+
+        self.intra_attn = intra_attn
+        if self.intra_attn:
+            self.feedforward_intra_attn = ntorch.nn.Linear(self.pretrained_emb.shape['embedding'], hidden_intra_attn).spec('embedding', 'hidden')
+            emb_dim = 2*emb_dim
+        self.feedforward_attn = ntorch.nn.Linear(emb_dim, hidden_attn).spec('embedding', 'hidden')
+        self.feedforward_aligned = ntorch.nn.Linear(2*emb_dim, hidden_aligned).spec('embedding', 'hidden')
+        self.feedforward_agg = ntorch.nn.Linear(2*hidden_aligned, len(LABEL.vocab)).spec('hidden', 'logit')
+
+        self.dropout = ntorch.nn.Dropout(dropout)
+
+    def forward(self, premise, hypothesis):
+        premise = self.embedding(premise)
+        premise = premise.rename('seqlen', 'seqlenPremise')
+        if self.intra_attn:
+            premise = self.intra_attn_layer(premise, 'seqlenPremise')
+        premise_hidden = self.feedforward_attn(premise).relu()
+        premise_hidden = self.dropout(premise_hidden)
+
+
+        hypothesis = self.embedding(hypothesis)
+        hypothesis = hypothesis.rename('seqlen', 'seqlenHypo')
+        if self.intra_attn:
+            hypothesis = self.intra_attn_layer(hypothesis, 'seqlenHypo')
+        hypothesis_hidden = self.feedforward_attn(hypothesis).relu()
+        hypothesis_hidden = self.dropout(hypothesis_hidden)
+
+        self.attn = premise_hidden.dot('hidden', hypothesis_hidden)
+        alpha = self.attn.softmax('seqlenHypo').dot('seqlenPremise', premise)
+        beta = self.attn.softmax('seqlenPremise').dot('seqlenHypo', hypothesis)
+
+        hypothesis_comparison = self.feedforward_aligned(ntorch.cat([alpha, hypothesis],'embedding')).relu().sum('seqlenHypo')
+        premise_comparison = self.feedforward_aligned(ntorch.cat([beta, premise],'embedding')).relu().sum('seqlenPremise')
+        agg = ntorch.cat([premise_comparison, hypothesis_comparison], 'hidden')
+        agg = self.feedforward_agg(agg)
+        agg = self.dropout(agg)
+
+        return agg
+
+    def intra_attn_layer(self, x, seqlen_dimname):
+        ## TODO: not sure about distance bias term
+
+        temp_dim = seqlen_dimname+'temp'
+        x_hidden1 = self.feedforward_intra_attn(x).relu()
+        x_hidden1 = self.dropout(x_hidden1)
+        x_hidden2 = x_hidden1.rename(seqlen_dimname, temp_dim)
+        intra_attn = x_hidden1.dot('hidden', x_hidden2).softmax(temp_dim)
+        x_aligned = intra_attn.dot(temp_dim, x)
+
+        # save attention distribution
+        if seqlen_dimname == "seqlenPremise":
+            self.intra_attn_premise = intra_attn
+        else:
+            self.intra_attn_hypo = intra_attn
+
+        return ntorch.cat([x, x_aligned], 'embedding')
