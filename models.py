cfed30f37fc86987a234397827c6bff0d0618c3b
Joe Davison <josephdavison@gmail.com>
2019-04-03 18:09:43 -0400

    final mixture models code

--- models.py
+++ models.py
@@ -1,38 +1,88 @@
 import torch
-# Text text processing library and methods for pretrained word embeddings
 import torchtext
 from torchtext.vocab import Vectors, GloVe
-
-# Named Tensor wrappers
 from namedtensor import ntorch, NamedTensor
 from namedtensor.text import NamedField
 import random
 import copy
+import numpy as np
+
+
+class VariationalEncoder(ntorch.nn.Module):
+
+    def __init__(self, TEXT, VOCAB, K, lstm_size, device='cpu'):
+        super().__init__()
+        self.pretrained_emb = TEXT.vocab.vectors.to(device)
+        self.embedding = ntorch.nn.Embedding.from_pretrained(
+            self.pretrained_emb.values, freeze=True).spec('seqlen','embedding')
+        emb_dim = self.pretrained_emb.shape['embedding']
+        self.lstm_prem = ntorch.nn.LSTM(emb_dim, lstm_size).spec('embedding', 'seqlen')
+        self.lstm_hyp = ntorch.nn.LSTM(emb_dim, lstm_size).spec('embedding', 'seqlen')
+        self.linear = ntorch.nn.Linear(lstm_size*2 + 1, K).spec('embedding', 'logprob')
+        self.to(device)
+        self.device = device
+    
+    def forward(self, premise, hypothesis, target):
+        prem = self.embedding(premise)
+        prem = self.lstm_prem(prem)[0][{'seqlen': -1}]
+        hyp = self.embedding(hypothesis)
+        hyp = self.lstm_hyp(hyp)[0][{'seqlen': -1}]
+        tar = ntorch.tensor(target.values.reshape(1, -1).to(torch.float32), names=('embedding', 'batch'))
+        flat = ntorch.cat([hyp, prem, tar], 'embedding')
+        out = self.linear(flat).log_softmax('logprob')
+        return out
+
 
 class LatentVariableMixtureModel(ntorch.nn.Module):
-    def __init__(self, model, experts, variational):
+
+    def __init__(self, model_const, experts, encoder=None):
         super().__init__()
-        try:
-            self.models = []
-            for _ in range(experts):
-                self.models.append(copy.deepcopy(model))
-        except RuntimeError:
-            raise RuntimeError("model must be newly instantiated")
+        self.models = torch.nn.ModuleList([model_const() for _ in range(experts)])
         self.experts = experts
-        self.variational = variational
+        self.q = encoder
 
-    def forward(self, premise, hypothesis):
-        if self.variational:
-            return self.sample(premise, hypothesis)
-        else:
-            return self.enumerate(premise, hypothesis)
+    def forward(self, premise, hypothesis, sample_size=None):
+        if sample_size:
+            assert self.q is not None, 'No encoder present'
+            model_inds = np.random.choice(self.experts, sample_size, replace=True)
+            models = [self.models[i] for i in model_inds]
+            return self.enumerate(premise, hypothesis, models=models)
+        return self.enumerate(premise, hypothesis)
 
-    def sample(premise, hypothesis):
-        pass
+    def reinforce(self, premise, hypothesis, target):
+        q_outputs = self.q(premise, hypothesis, target).log_softmax('logprob')
+        log_q, model_inds = q_outputs.topk('logprob', 1)
+        model_inds = set([int(i.item()) for i in model_inds.values.reshape(-1)])
+        elbos = []
+        grad_fn_val = torch.tensor(0., device=self.q.device)
+        for i in model_inds:
+            inds = (q_outputs.topk('logprob', 1)[1] == i).nonzero().rename('elements', 'batch')[{'inputdims': 0}]
+            mod_premise = premise.index_select('batch', inds)
+            mod_hypothesis = hypothesis.index_select('batch', inds)
+            mod_target = target.index_select('batch', inds)
+            mod_qout = q_outputs.index_select('batch', inds)
+            elbo, grad = self.reinforce_batch(mod_premise, mod_hypothesis, mod_target, self.models[i], mod_qout)
+            grad_fn_val = grad_fn_val + grad
+            for _ in range(len(mod_premise)):
+                elbos.append(elbo)
+        grad_fn_val.backward()
+        return np.mean(elbos)
 
-    def enumerate(self, premise, hypothesis):
+    def reinforce_batch(self, premise, hypothesis, target, model, q_outputs):
+        """ Calculates the gradients of the models and returns the ELBO. """
+        log_q, _ = q_outputs.topk('logprob', 1)
+        outputs = model(premise, hypothesis)
+        log_p = outputs.log_softmax('logit').rename('logit', 'logprob')
+        kl_term = (q_outputs.exp() * (q_outputs - torch.tensor(1 / self.experts).log())).sum('logprob')
+        p = log_p.values.gather(1, target.values.reshape(-1, 1))
+        elbo = p.mean() - kl_term.values.mean()
+
+        grad_fn = (kl_term.values - p - (p.detach() * log_q.values)).mean()
+        return elbo.item(), grad_fn
+
+    def enumerate(self, premise, hypothesis, models=None):
         predictions = []
-        for model in self.models:
+        for model in (models or self.models):
             predictions.append(model(premise, hypothesis))
         return (
             ntorch.stack(predictions, "experts")
@@ -41,6 +91,70 @@ class LatentVariableMixtureModel(ntorch.nn.Module):
             .log()
             .rename('logit','logprob')
         )
+
+    def uniform_fit(self, train_iter, val_iter, epochs, lr, verbose):
+        crit = torch.nn.NLLLoss()
+        self.losses = []
+        self.val_losses = []
+        opt = torch.optim.Adagrad(self.parameters(), lr=lr)
+        for e in range(epochs):
+            for ibatch, batch in enumerate(train_iter):
+                opt.zero_grad()
+                pred = self.forward(batch.premise, batch.hypothesis)
+                loss = crit(
+                    pred.transpose('batch', 'logprob').values,
+                    batch.label.transpose('batch').values)
+                self.losses.append(loss.item())
+                loss.backward()
+                opt.step()
+                if verbose and ibatch % verbose == 0 and ibatch > 0:
+                    print(f"batch: {ibatch}", end='')
+                    print(f', loss: {np.mean(self.losses[-verbose:])}')
+            val_loss = self.eval(val_iter)
+            self.val_losses.append(val_loss)
+            if verbose:
+                print(f'epoch: {e}, loss: {self.val_losses[-1]}')
+
+    def eval(self, iter):
+        crit = torch.nn.NLLLoss()
+        losses = []
+        for ibatch, batch in enumerate(iter):
+            pred = self.forward(batch.premise, batch.hypothesis)
+            loss = crit(
+                pred.transpose('batch', 'logprob').values,
+                batch.label.transpose('batch').values)
+            losses.append(loss.item())
+        return np.mean(losses)
+
+
+    def fit(self, train_iter, val_iter=[], epochs=1, lr=.001, verbose=False):
+        if not self.q:
+            self.uniform_fit(train_iter, val_iter, epochs, lr, verbose)
+            return
+        crit = torch.nn.NLLLoss()
+        self.elbos = []
+        self.losses = []
+        self.val_losses = []
+        opt = torch.optim.Adagrad(self.parameters(), lr=lr)
+        for e in range(epochs):
+            for ibatch, batch in enumerate(train_iter):
+                opt.zero_grad()
+                elbo = self.reinforce(batch.premise, batch.hypothesis, batch.label)
+                pred = self.forward(batch.premise, batch.hypothesis)
+                loss = crit(
+                    pred.transpose('batch', 'logprob').values,
+                    batch.label.transpose('batch').values)
+                self.elbos.append(elbo)
+                self.losses.append(loss.item())
+                opt.step()
+                if verbose and ibatch % verbose == 0 and ibatch > 0:
+                    print(f"batch: {ibatch}", end='')
+                    print(f', elbo: {np.mean(self.elbos[-verbose:])}, loss: {np.mean(self.losses[-verbose:])}')
+            val_loss = self.eval(val_iter)
+            self.val_losses.append(val_loss)
+            if verbose:
+                print(f'epoch: {e}, loss: {self.val_losses[-1]}')
+
 
 class AttentionModel(ntorch.nn.Module):
     def __init__(self,
@@ -74,6 +188,7 @@ class AttentionModel(ntorch.nn.Module):
         self.feedforward_aligned = MLP(2*emb_dim, hidden_aligned, 'embedding', 'hidden', self.dropout)
         self.feedforward_agg = MLP(2*hidden_aligned, hidden_final, 'hidden', 'final',self.dropout)
         self.final_linear = ntorch.nn.Linear(hidden_final, len(LABEL.vocab)).spec('final','logit')
+        self.to(device)
 
     def forward(self, premise, hypothesis):
         premise = self.embedding(premise)
@@ -159,7 +274,7 @@ class AttentionModel(ntorch.nn.Module):
                 optimizer.step()
 
                 running_loss += loss.item()
-                if i % interval == 0 and verbose:
+                if i % interval == 0 and i > 0 and verbose:
                     print(f"batch: {i}, loss: {running_loss/interval}")
                     running_loss = 0
             if verbose and val_iter is not None:
